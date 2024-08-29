daf048421ba9e9ad95de6d286ce469177f36f1d3
Atikur Rhaman <designletter999@gmail.com>
2024-08-29 21:25:15 +0600

    nhh

---
+++ Module 4 jQuery Basic Concepts/part-1/script.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//     $('#ami').hide();
+// });
+// $(document).ready(function() {
+//     $('[href]').hide();
+// });
+$(document).ready(function() {
+    $('button').click(function() {
+        $('h2').hide();
+    });
+});
+
+// $('p').hide();
