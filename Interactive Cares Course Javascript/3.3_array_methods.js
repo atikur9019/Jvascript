326f8f3b0c2e0d3c429eb535648246fc8091fccd
Atikur Rhaman <designletter999@gmail.com>
2024-08-31 01:55:49 +0600

    +file++

---
+++ Interactive Cares Course Javascript/3.3_array_methods.js
@@ -0,0 +1,16 @@
+let persons = ['Atikur', 'Rhaman', 'Atik'];
+
+// let  ignored = persons.pop(); //delete last element
+
+// console.log(ignored);
+// console.log(persons.length);
+
+// persons[3] = 'Jane'
+
+// persons.push('Tamim'); //add last element
+
+// persons.shift(); //delete fast element
+
+// persons.unshift('Md.');
+
+// console.log(persons);
