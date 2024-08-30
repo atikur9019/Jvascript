7ac445b965dae5a0e3c36f65df7d7cc40cecb371
Atikur Rhaman <designletter999@gmail.com>
2024-08-30 20:44:23 +0600

    data type 1

---
+++ Interactive Cares Course Javascript/1.6_data.js
@@ -0,0 +1,9 @@
+/**************************Data**************************/
+
+/**
+ * "I am Atikur Rhaman Atik" // (String)
+ * 14 // (Number)
+ * true // (Boolean)
+ * 45.34 // (Float)
+ * ["fruits", "vegetables", "drinks",] // (Array)
+ */
