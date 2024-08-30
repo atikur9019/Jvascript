117fdb89402b375a3545be91047ce0084f521dfa
Atikur Rhaman <designletter999@gmail.com>
2024-08-30 22:48:36 +0600

    add file 4

---
+++ Interactive Cares Course Javascript/2.6_number_method.js
@@ -0,0 +1,21 @@
+// toString
+// toExponential
+// toFixed
+// math.round()
+// math.ceil()
+// math.floor()
+// math.pow()
+// math.random()
+
+let num = 12.467989;
+let num2 = 4;
+
+// console.log(Math.round(num));
+// console.log(Math.ceil(num));
+// console.log(Math.floor(num));
+// console.log(Math.pow(8, 2));
+// console.log(Math.random() * 100);
+// console.log(Math.round(Math.random() * 100));
+// console.log(Math.PI)
+// console.log(Math.max(1, 1, 4, 14, 1, 4, 1, 4, 21, 4, 21));
+// console.log(Math.min(1, 1, 4, 14, 1, 4, 1, 4, 21, 4, 21));
