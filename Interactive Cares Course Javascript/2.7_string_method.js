7884bea1379c17cf0ad95c155490bd1a24dd835c
Atikur Rhaman <designletter999@gmail.com>
2024-08-30 23:33:48 +0600

    file add  5

---
+++ Interactive Cares Course Javascript/2.7_string_method.js
@@ -0,0 +1,16 @@
+let str = 'Atikur Rhaman Atik';
+let koma = 'Atikur,Rhaman,Atik';
+let srta = '     Atikur Rhaman Atik';
+
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.slice(-4));
+// console.log(str.substring(0, 6));
+// console.log(str.replace('Rhaman Atik', ''));
+// console.log('Atikur'.concat(' ', 'World!'));
+// console.log(str + '!');
+// console.log(srta.trim());
+// console.log(str.charAt(0));
+// console.log(str.split(' '));
+// console.log(koma.split(','));
