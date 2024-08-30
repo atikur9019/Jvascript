4ccecfe6c840dcbba457276a2b0060f5c3b67c1d
Atikur Rhaman <designletter999@gmail.com>
2024-08-30 21:31:58 +0600

    2.4_implicit.js add

---
+++ Interactive Cares Course Javascript/2.4_implicit.js
@@ -0,0 +1,25 @@
+// String
+// Number
+// Boolean
+
+let a = 15; // Number
+let b = '10'; // String
+// let bb = '10ab'; // String
+
+// console.log(a + b);
+// console.log(a - b);
+  //  console.log(Number (bb)); 
+  //  console.log(parseInt (bb)); 
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a > b);
+
+
+// let x = 'Hello' && 123;
+// let x = null && 123;
+let x = null || 123;
+
+// true & true -> true
+// true & false -> false
+
+console.log(x);
