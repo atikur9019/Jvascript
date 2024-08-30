b0707df3eddf91aad4f0767c9998d1c9d62de20d
Atikur Rhaman <designletter999@gmail.com>
2024-08-30 21:08:38 +0600

    data_type

---
+++ Interactive Cares Course Javascript/2.2_data_types.js
@@ -0,0 +1,35 @@
+/**
+ * 1. Primitive values
+ *  a. String
+ *  b. Number
+ *  c. Boolean
+ *  d. undefined
+ *  e. null
+ *  f. BigInteger
+ *  g. Symbol
+ * 2. Object
+ * 
+ * 
+ * check: typeof
+ */
+
+let str = "Atikur Rhaman Atik"; // String
+let num = 4634564; // Number
+
+// initiate
+let x; // undefined
+let und; // undefined
+// assign
+und = "Hello world!";
+
+let nl = null; // null
+
+// array
+
+let arr = ['One', 'Two', 'Three']; // array
+
+// Boolean
+
+let bool = true; // boolean
+
+console.log(typeof bool);
