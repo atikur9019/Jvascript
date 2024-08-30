a7a87cc8d5a48c5fc4177400f19ec13f177b848a
Atikur Rhaman <designletter999@gmail.com>
2024-08-30 15:02:01 +0600

    HI

---
+++ Interactive Cares Course Javascript/1.3_variable.js
@@ -0,0 +1,15 @@
+var firstName = 'John';
+var age = 14;
+let lastName = 'Setu';
+
+
+console.log(firstName); // John
+console.log(age)
+console.log(lastName);
+
+firstName = 'Atikur'; // Atikur
+
+const PI = 3.1416;
+// PI = 5.3;
+
+console.log(PI);
