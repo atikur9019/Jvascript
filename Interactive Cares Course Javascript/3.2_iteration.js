b9148d5e590e2ed7fc901361b8b6437263f68b36
Atikur Rhaman <designletter999@gmail.com>
2024-08-31 01:12:09 +0600

    hh

--- Interactive Cares Course Javascript/3.2_iteration.js
+++ Interactive Cares Course Javascript/3.2_iteration.js
@@ -1,7 +1,13 @@
 let num = [1, 2, 3, 4, 5, 6];
+let person = ['Atikur', 'Rhaman', 'Atik'];
 
 // for loop
-for (let i= 0; i < num.length; i++) {
-   console.log(num[i]);
-}
+// for (let i= 0; i < person.length; i++) {
+//    console.log(person[i]);
+// }
+
 //forEach
+
+num.forEach(function (item, index, arr) {
+    console.log(item, index);
+});
