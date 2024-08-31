73376532dea3daa7b6230add60ccae419fe913fa
Atikur Rhaman <designletter999@gmail.com>
2024-08-31 15:22:01 +0600

    ad file 3

--- Interactive Cares Course Javascript/4.3_assignment.js
+++ Interactive Cares Course Javascript/4.3_assignment.js
@@ -1,3 +1,5 @@
+let number1 = 11;
+let number2 = 2;
 let num1 = 10;
 let num2 = 15;
 
@@ -5,6 +7,13 @@ let num2 = 15;
 // num1 += num2;
 // num1 = num1 - num2;
 // num1 -= num2;
-num1 =  num1 * num2;
+// num1 =  num1 * num2;
+// num1 *=  num2;
+// num1 = num1 / num2;
+// num1 /= num2;
+// number1 = number1 % number2;
+// number1 %= number2;
+// number1 = number1 ** number2;
+number1 **= number2;
 
-console.log(num1);
+console.log(number1);
