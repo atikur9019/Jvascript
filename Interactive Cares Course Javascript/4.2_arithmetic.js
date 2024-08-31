9ad307ae26ed8fae41e961c49cee35ebaa700d9d
Atikur Rhaman <designletter999@gmail.com>
2024-08-31 14:51:39 +0600

    f

---
+++ Interactive Cares Course Javascript/4.2_arithmetic.js
@@ -0,0 +1,19 @@
+let a = 10;
+let a1 = 11;
+let b = 2;
+// a1++;
+// a1--;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// console.log(a ** b);
+// console.log(a1 % b);
+// console.log(a1);
+//   console.log(a1++);
+//   console.log(++a1);
+// console.log(a1);
+    //  console.log(a1--);
+     console.log(--a1);
