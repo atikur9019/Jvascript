bbef80ec01716475e600f439c3481a7e789bf4b5
Atikur Rhaman <designletter999@gmail.com>
2024-08-31 16:17:06 +0600

    push

---
+++ Interactive Cares Course Javascript/4.4_comparison.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = '10';
+
+// console.log(num1 === num2);
+// console.log(num1 !== num2);
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 <= num2);
+// console.log(num1 >= num2);
+// console.log(num1 >= num2);
+
+let str = num1 > num2 ? 'Atik' : 'Atikur';
+
+console.log (str);
