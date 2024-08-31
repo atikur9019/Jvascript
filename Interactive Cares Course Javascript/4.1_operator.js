db050bba2f5ce69a65dadc66180b00b85bd74f3f
Atikur Rhaman <designletter999@gmail.com>
2024-08-31 14:33:29 +0600

    hh

--- Interactive Cares Course Javascript/4.1_operator.js
+++ Interactive Cares Course Javascript/4.1_operator.js
@@ -1,1 +1,11 @@
 console.log(5 + 10 );
+let x = 10;
+
+/**
+ * 1.Arithmetic Operator
+ * 2.Assignment Operator
+ * 3.Comparison Operator
+ * 4.Logical operator
+ * 5.Bitwise Operator
+ * 6.typeof
+ */
